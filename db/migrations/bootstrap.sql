a450dd586eee8c82eb86b5c8e3426fb3de97a0ec
Stuart Branham <stuart.branham@gmail.com>
2015-05-25 02:35:19 -0700

    LESS task.

--- db/migrations/bootstrap.sql
+++ db/migrations/bootstrap.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `version` (
+CREATE TABLE if not exists `version` (
   `version` int(11) NOT NULL DEFAULT '0',
   `migrating` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var livereload = require('gulp-livereload');
+var less = require('gulp-less');
+
+var path = require('path');
 var config = require('./config.json');
+
+gulp.task('less', function () {
+  return gulp.src('./less/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'include') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
 
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
     script: 'bin/www',
-    ext: 'js json css hbs',
+    ext: 'js json less hbs',
   }).on('restart', function () {
     setTimeout(function () {
       livereload.changed(__dirname);
     }, 500);
   });
+  gulp.watch([path.join(__dirname, 'less', '**', '*.less')], ['less']);
 });
 
 gulp.task('migrate', function () {
   var version = require('./db/version');
-  console.log(version.error, version);
   if (version.error) {
     console.error(version.error);
   } else {
@@ -26,6 +37,7 @@ gulp.task('migrate', function () {
 });
 
 gulp.task('default', [
+  'less',
   'migrate',
   config.task
 ]);
